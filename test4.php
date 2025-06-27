cdfd13d850184bc2f75c5c6beff69efc83098851
donghyun-kim94 <gliskim00@gmail.com>
2025-06-27 10:18:43 +0900

    DongTest4

--- test4.php
+++ test4.php
@@ -9,7 +9,7 @@
   <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
   <style>
-  /* Makedfddfdfdfdf the image fully responsive */
+  /* Makedfddfdfdfdf1111111111111111 the image fully responsive */
   .carousel-inner img {
     width: 100%;
     height: 100%;
